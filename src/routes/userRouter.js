b19f029142884a438ae8c416b02ba4593584c2ff
yexnsxo <yslove829@naver.com>
2026-01-27 12:09:05 +0900

    useRouter.js 오류 수정

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,13 +1,13 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const userController = require('../controllers/userController');
-const { verifyToken } = require('../middlewares/authMiddleware');
+const userController = require("../controllers/userController");
+const { verifyToken } = require("../middlewares/authMiddleware");
 
 // 모든 요청에 토큰 필요
 router.use(verifyToken);
 
-router.get('/profile', userController.getProfile);
-router.put('/profile', userController.updateProfile);
-router.put('/profile/sports/:sportType', userController.updateSportProfile);
+router.get("/profile", userController.getMe);
+router.put("/profile", userController.updateProfile);
+router.put("/profile/sports/:sportType", userController.updateSportProfile);
 
 module.exports = router;
